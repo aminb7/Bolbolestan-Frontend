3344df744a53e13d26b38a7d5bfd5887857a2fcd
amin <m.baqershahi@gmail.com>
2021-03-04 17:05:43 +0330

    Add HttpGetter.

--- src/main/java/org/ie/bolbolestan/BolbolestanCommandLine.java
+++ src/main/java/org/ie/bolbolestan/BolbolestanCommandLine.java
@@ -1,35 +1,11 @@
 package org.ie.bolbolestan;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.ie.bolbolestan.system.Bolbolestan;
+import org.ie.bolbolestan.utility.HttpGetter;
 
-import java.util.Scanner;
+import java.io.IOException;
 
 public class BolbolestanCommandLine {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		Bolbolestan bolbolestan = new Bolbolestan();
-		while (true) {
-			String line = scanner.nextLine();
-			if (line.isEmpty())
-				continue;
-
-			String[] splitLine = line.split(" ", 2);
-			String command = splitLine[0];
-			String data = "";
-
-			if (splitLine.length > 1)
-				data = splitLine[1];
-
-			ObjectNode message = bolbolestan.execute(command, data);
-
-			try {
-				System.out.println(new ObjectMapper().writeValueAsString(message));
-			} catch (JsonProcessingException e) {
-				e.printStackTrace();
-			}
-		}
+	public static void main(String[] args) throws IOException, InterruptedException {
+		System.out.println(HttpGetter.Get("http://138.197.181.131:5000/api/courses"));
 	}
 }
