ef42670b39621bc217c725a2cb08047125b6e08f
aminb7 <45150807+aminb7@users.noreply.github.com>
2021-04-25 15:47:51 +0430

    Make header and footer clean.

--- src/main/resources/static/js/footer.js
+++ src/main/resources/static/js/footer.js
@@ -1,30 +1,46 @@
+function ApplicationIcon(props) {
+    return (
+        <li className="nav-item">
+            <a className="nav-link"><img className="footer_icon_size invert_image_color" src={props.src} alt={props.alt}></img></a>
+        </li>
+    );
+}
+
+function CopyRightIcon() {
+    return (
+        <img className="copy_right_icon_size invert_image_color" src="../images/icons/006-copyright.png" alt="copy right"></img>
+    );
+}
+
+function FooterText() {
+    return (
+        <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <div className="navbar-nav mr-auto order-0 nav-item active">
+                <a className="nav-link footer_text">دانشگاه تهران - سامانه جامع بلبل‌ستان</a>
+            </div>
+        </div>
+    );
+}
+
+function ApplicationIcons(props) {
+    return (
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto order-3">
+                <ApplicationIcon src="../images/icons/003-twitter-logo-on-black-background.png" alt="twitter"/>
+                <ApplicationIcon src="../images/icons/004-instagram.png" alt="instagram"/>
+                <ApplicationIcon src="../images/icons/002-linkedin-logo.png" alt="linkedin"/>
+                <ApplicationIcon src="../images/icons/005-facebook.png" alt="facebook"/>
+            </ul>
+        </div>
+    );
+}
+
 function BolbolestanFooter() {
     return (
         <nav className="navbar navbar-expand-md sticky-top footer_color">
-            <img className="copy_right_icon_size invert_image_color" src="../images/icons/006-copyright.png" alt="copy right"></img>
-
-            <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-                <div className="navbar-nav mr-auto order-0 nav-item active">
-                    <a className="nav-link footer_text">دانشگاه تهران - سامانه جامع بلبل‌ستان</a>
-                </div>
-            </div>
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto order-3">
-                    <li className="nav-item">
-                        <a className="nav-link"><img className="footer_icon_size invert_image_color" src="../images/icons/003-twitter-logo-on-black-background.png" alt="twitter"></img></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link"><img className="footer_icon_size invert_image_color" src="../images/icons/004-instagram.png" alt="instagram"></img></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link"><img className="footer_icon_size invert_image_color" src="../images/icons/002-linkedin-logo.png" alt="linkedin"></img></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link"><img className="footer_icon_size invert_image_color" src="../images/icons/005-facebook.png" alt="facebook"></img></a>
-                    </li>
-                </ul>
-            </div>
+            <CopyRightIcon />
+            <FooterText />
+            <ApplicationIcons />
         </nav>
     );
 }
--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,30 +1,58 @@
+function BolbolestanLogo() {
+    return (
+        <img className="navbar-brand header_logo" src="../images/logo.png" alt="logo"></img>
+    );
+}
+
+function HeaderText() {
+    return (
+        <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <ul className="navbar-nav mr-auto order-0">
+                <li className="nav-item active">
+                    <a className="nav-link header_text">خانه</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link header_text">انتخاب واحد</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+function LogoutText() {
+    return (
+        <li className="nav-item active">
+            <a className="nav-link header_logout_text">خروج</a>
+        </li>
+    );
+}
+
+function LogoutIcon() {
+    return (
+        <li className="nav-item">
+            <a className="nav-link"><img className="header_logout_icon" src="../images/icons/001-log-out.png" alt="log out"></img></a>
+        </li>
+    );
+}
+
+function LogoutSection() {
+    return (
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto order-3">
+                <LogoutText />
+                <LogoutIcon />
+            </ul>
+        </div>
+    );
+}
+
 function BolbolestanHeader() {
     return (
         <div className="row sticky-top header_border">
             <nav className="col-lg-12 navbar navbar-expand-md header_color">
-                <img className="navbar-brand header_logo" src="../images/logo.png" alt="logo"></img>
-
-                    <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-                        <ul className="navbar-nav mr-auto order-0">
-                            <li className="nav-item active">
-                                <a className="nav-link header_text">خانه</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link header_text">انتخاب واحد</a>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ml-auto order-3">
-                            <li className="nav-item active">
-                                <a className="nav-link header_logout_text">خروج</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link"><img className="header_logout_icon" src="../images/icons/001-log-out.png" alt="log out"></img></a>
-                            </li>
-                        </ul>
-                    </div>
+                <BolbolestanLogo />
+                <HeaderText />
+                <LogoutSection />
             </nav>
         </div>
     );
