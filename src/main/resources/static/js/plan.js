16f77648d42e432700b60c41a0f0c9251f9d30fd
aminb7 <45150807+aminb7@users.noreply.github.com>
2021-04-26 18:34:40 +0430

    Complete plan page

--- src/main/resources/static/js/plan.js
+++ src/main/resources/static/js/plan.js
@@ -38,9 +38,76 @@ function DaysRow(props) {
     );
 }
 
+function getTimeIndex(time) {
+    var timeIndex = time.replaceAll(':', '');
+    timeIndex = timeIndex.substring(0, timeIndex.length - 2);
+    timeIndex = (Number(timeIndex.substring(timeIndex.length - 2)) / 60)
+        + Number(timeIndex.substring(0, timeIndex.length - 2)) - 7;
+
+    return timeIndex;
+}
+
+function haveSameTime(start, end, tableTimeNum) {
+    var startIndex = getTimeIndex(start);
+    return (startIndex >= tableTimeNum) && (startIndex < tableTimeNum + 1);
+}
+
+function haveSameDay(courseDays, tableDayNumber) {
+    var daysNumbers = courseDays.map(function (day){
+        switch (day) {
+            case "Saturday": return 1;
+            case "Sunday": return 2;
+            case "Monday": return 3;
+            case "Tuesday": return 4;
+            case "Wednesday": return 5;
+            case "Thursday": return 6;
+        }
+    })
+
+    return daysNumbers.includes(Number(tableDayNumber));
+}
+
+function courseTypeStyle(type) {
+    switch (type) {
+        case "Asli": return "main_course_border";
+        case "Umumi": return "general_course_border";
+        case "Takhasosi": return "specialized_course_border";
+        case "Paaye": return "basic_course_border";
+    }
+}
+
 function TableCell(props) {
+    if (!props.isLoggedin)
+        return (<p></p>);
+
+    var matchedCourse;
+    var isMatched = false;
+    var coursesList = Object.values(props.courses);
+    for (var i = 0; i < coursesList.length; i++) {
+        if (haveSameDay(coursesList[i].course.classTime.days, props.dayNum)
+                && haveSameTime(coursesList[i].course.classTime.start, coursesList[i].course.classTime.end, props.timeNum)) {
+            isMatched = true;
+            matchedCourse = coursesList[i];
+            break;
+        }
+    }
+
+    if (!isMatched)
+        return (<p></p>);
+
+    var startIndex = getTimeIndex(matchedCourse.course.classTime.start);
+    var endIndex = getTimeIndex(matchedCourse.course.classTime.end);
+
+    var filteredStart = String(matchedCourse.course.classTime.start).substr(0, String(matchedCourse.course.classTime.start).length - 3);
+    var filteredEnd = String(matchedCourse.course.classTime.end).substr(0, String(matchedCourse.course.classTime.end).length - 3);
+    var height = (endIndex - startIndex) * 48;
+
+    var style = (startIndex - Math.trunc(startIndex) === 0) ? {height: (String(height)+"px")} : {height: (String(height)+"px"), top: "30px"};
+
     return (
-        <p></p>
+        <p className={courseTypeStyle(matchedCourse.course.type)} style={style}>
+            {filteredStart + "-" + filteredEnd}<br />{matchedCourse.course.name}<br />{matchedCourse.course.type}
+        </p>
     );
 }
 
@@ -60,7 +127,7 @@ class WeeklySchedule extends React.Component {
     }
 
     render() {
-        const timeSlots = ["۹:۰۰ - ۸:۰۰", "۱۰:۰۰ - ۹:۰۰", "۱۱:۰۰ - ۱۰:۰۰", "۱۲:۰۰ - ۱۱:۰۰", "۱۳:۰۰ - ۱۲:۰۰",
+        const timeSlots = ["۸:۰۰ - ۷:۰۰", "۹:۰۰ - ۸:۰۰", "۱۰:۰۰ - ۹:۰۰", "۱۱:۰۰ - ۱۰:۰۰", "۱۲:۰۰ - ۱۱:۰۰", "۱۳:۰۰ - ۱۲:۰۰",
             "۱۴:۰۰ - ۱۳:۰۰", "۱۵:۰۰ - ۱۴:۰۰", "۱۶:۰۰ - ۱۵:۰۰", "۱۷:۰۰ - ۱۶:۰۰", "۱۸:۰۰ - ۱۷:۰۰"];
 
         const dayNumbers = [1,2,3,4,5,6];
@@ -75,7 +142,8 @@ class WeeklySchedule extends React.Component {
                                 <td>{timeSlot}</td>
                                 {dayNumbers.map((dayNumber) => (
                                     <td>
-                                        <TableCell dayNum={dayNumber} timeNum={index}/>
+                                        <TableCell dayNum={dayNumber} timeNum={index}
+                                                   courses={this.state.selectedCourses} isLoggedin={this.state.isLoggedin}/>
                                     </td>
                                 ))}
                             </tr>
